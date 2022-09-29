c121170cc8774b2435d3c4e62b7d8790d5a0976a
hcheng158 <hcheng158@yahoo.com>
2022-09-28 17:48:18 -0700

    second commit

---
+++ src/TestFinal/finalclass002.java
@@ -0,0 +1,10 @@
+package TestFinal;
+
+public class finalclass002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("new code 1 yo yo yo");
+	}
+
+}
